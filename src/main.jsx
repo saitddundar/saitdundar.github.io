1757444a7624a32dbcee38f35a26a9ca3d534fa9
Sait Dundar <saitdndr51@gmail.com>
2025-11-04 01:06:49 +0300

    fix: Correct import paths in App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/global.css'
-import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // <-- BU SATIRI EKLE
+import App from './App';
+import './index.css'; // Veya diğer stiller
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HashRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter> {/* <-- BU SARMALAYICI ŞART */}
       <App />
-    </HashRouter>
-  </StrictMode>,
-)
+    </BrowserRouter>
+  </React.StrictMode>
+);
