f8c570eef0331d11328a20e1690ef054c1438648
Sait Dundar <saitdndr51@gmail.com>
2025-11-04 00:51:43 +0300

    fix: Correct import paths in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import Home from './pages/HomePage.jsx';
 import About from './pages/About.jsx';
 import Projects from './pages/Projects.jsx';
 import Contact from './pages/Contact.jsx';
@@ -8,10 +8,10 @@ import Contact from './pages/Contact.jsx';
 function App() {
   return (
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/saitdundar.github.io/" element={<Home />} />
+        <Route path="/saitdundar.github.io/about" element={<About />} />
+        <Route path="/saitdundar.github.io/projects" element={<Projects />} />
+        <Route path="/saitdundar.github.io/contact" element={<Contact />} />
       </Routes>
   );
 }
