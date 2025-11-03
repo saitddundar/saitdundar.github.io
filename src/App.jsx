ae03f842b31b7b34fc4e5a5644dfa21c1c18749d
Sait Dundar <saitdndr51@gmail.com>
2025-11-04 01:00:26 +0300

    fix: Correct import paths in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/HomePage.jsx';
-import About from './sections/Section2/Section2.jsx';
-import Projects from './sections/Section3/Section3.jsx';
-import Contact from './footer/Footer.jsx';
+import React from 'react'
+import MainLayout from './components/Layout/MainLayout'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
-      <Routes>
-        <Route path="/saitdundar.github.io/" element={<Home />} />
-        <Route path="/saitdundar.github.io/about" element={<About />} />
-        <Route path="/saitdundar.github.io/projects" element={<Projects />} />
-        <Route path="/saitdundar.github.io/contact" element={<Contact />} />
-      </Routes>
-  );
+    <MainLayout>
+      <HomePage />
+    </MainLayout>
+  )
 }
 
-export default App;
+export default App
