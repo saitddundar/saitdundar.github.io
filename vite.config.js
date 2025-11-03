ddf15050b6f165dc122babf03784c54c786ec5bf
Sait Dundar <saitdndr51@gmail.com>
2025-11-03 22:44:40 +0300

    configure vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/saitddundar.github.io',
+  base: '/saitdundar.github.io',
   build: {
     outDir: 'dist'
   }
